e9ba4c338f53eb1126bb03fa2cc1a122ddfc1298
NguyenMinhNhat <72795828+Nguyen-Minh-Nhat@users.noreply.github.com>
2023-09-08 01:33:06 +0700

    feat: Add user moduleA new feature is added to the codebase, specifically the user module

---
+++ src/users/schemas/user.schema.ts
@@ -0,0 +1,53 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+export enum UserStatus {
+  ACTIVE = 'ACTIVE',
+  BANNED = 'BANNED',
+}
+
+// @Schema({ _id: false })
+
+@Schema({
+  timestamps: true,
+})
+export class User {
+  _id: mongoose.Schema.Types.ObjectId;
+  @Prop({ type: String, unique: true })
+  username: string;
+  @Prop({ type: String })
+  bio: string;
+
+  @Prop({ type: String })
+  email: string;
+
+  @Prop({ type: String })
+  password: string;
+
+  @Prop({ type: String })
+  avatar: string;
+
+  @Prop({
+    type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    default: [],
+  })
+  blacklist: User[];
+
+  @Prop({
+    type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    default: [],
+  })
+  following: User[];
+
+  @Prop({
+    type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    default: [],
+  })
+  followers: User[];
+  @Prop({ type: String, default: UserStatus.ACTIVE })
+  status: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
