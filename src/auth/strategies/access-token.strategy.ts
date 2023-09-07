692bb182a386383339ffd9d1aa0cab182e5830e8
NguyenMinhNhat <72795828+Nguyen-Minh-Nhat@users.noreply.github.com>
2023-09-08 01:32:12 +0700

    feat: add access token and refresh token guards and strategies

---
+++ src/auth/strategies/access-token.strategy.ts
@@ -0,0 +1,21 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+import { Injectable } from '@nestjs/common';
+import { JwtPayload } from '../types/jwt-payload.type';
+import { PassportStrategy } from '@nestjs/passport';
+import { envConfig } from 'src/configs/env.config';
+
+@Injectable()
+export class AccessTokenStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: envConfig.jwt.accessToken.secret,
+    });
+  }
+
+  async validate(payload: JwtPayload) {
+    return payload;
+  }
+}
---
+++ src/auth/strategies/refresh-token.strategy.ts
@@ -0,0 +1,30 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+
+import { JwtPayload } from '../types/jwt-payload.type';
+import { PassportStrategy } from '@nestjs/passport';
+import { Request } from 'express';
+import { envConfig } from 'src/configs/env.config';
+
+@Injectable()
+export class RefreshTokenStrategy extends PassportStrategy(
+  Strategy,
+  'jwt-refresh',
+) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: envConfig.jwt.refreshToken.secret,
+      passReqToCallback: true,
+    });
+  }
+
+  async validate(req: Request, payload: JwtPayload) {
+    const refreshToken = req.get('Authorization')?.split(' ')[1];
+    if (!refreshToken) throw new ForbiddenException('Refresh token malformed');
+    return {
+      ...payload,
+      refreshToken,
+    };
+  }
+}
