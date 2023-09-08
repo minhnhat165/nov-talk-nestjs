f749c4bdc856ac4900051427f5355f3958fe6f45
NguyenMinhNhat <72795828+Nguyen-Minh-Nhat@users.noreply.github.com>
2023-09-08 08:41:45 +0700

    feat(users): add find and findById methods

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,8 @@
 import { HttpException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { User } from './schemas/user.schema';
+import { Model, ObjectId } from 'mongoose';
+import { User, UserStatus } from './schemas/user.schema';
+import { FindParams } from 'src/common/types';
 
 @Injectable()
 export class UsersService {
@@ -18,6 +19,31 @@ export class UsersService {
       .lean();
     if (!user) {
       throw new HttpException('User not found', 404);
+    }
+    return user;
+  }
+  async find({ q, limit }: FindParams): Promise<User[]> {
+    const users = await this.userModel
+      .find({
+        $or: [
+          { name: { $regex: q, $options: 'i' } },
+          { username: { $regex: q, $options: 'i' } },
+        ],
+        status: UserStatus.ACTIVE,
+      })
+      .limit(limit)
+      .select({
+        name: true,
+        username: true,
+        avatar: true,
+      })
+      .lean();
+    return users;
+  }
+  async findById(id: ObjectId | string) {
+    const user = await this.userModel.findById(id).lean();
+    if (!user) {
+      throw new HttpException(`User ${id} not found`, 404);
     }
     return user;
   }
